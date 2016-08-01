827d73c544ffb494656cb808078ef48d25ef1a84
notjrbauer <notjrbauer@gmail.com>
2016-08-01 06:39:56 -0700

    try catch due to it being both sync and async ._.

--- server.js
+++ server.js
@@ -9,110 +9,134 @@ var analytics = new Analytics('i6aoC3CdUjS4BvSOvmJQguLBAlvzt6kG')
 var app = express()
 var PORT = process.argv.PORT || 4001
 
-app.use(bodyParser.json())
 app.use(cors())
 app.use(bodyParser.urlencoded({
   extended: true
 }))
+app.use(bodyParser.json())
 
 app.post('/identify', function (req, res, next) {
 
   // already sanitized
   var body = req.body
+  body.timestamp = new Date(body.timestamp)
 
-  analytics.identify(body, function (err, response, data) {
-    debug('Identify', err, response)
-  })
-  res.send({
-    event: 'identify',
-    message: 'success'
-  })
+  try {
+    analytics.identify(body, function (err, response, data) {
+      debug('Identify', err, response)
+      debug('Data', data)
+
+      res.send({
+        event: 'identify',
+        message: 'success'
+      })
+    })
+  } catch (err) {
+    return res.send(err)
+  }
 })
 
 app.post('/track', function (req, res, next) {
 
   // already sanitized
   var body = req.body
-  console.log('THE BODY IS', req.body)
+  body.timestamp = new Date(body.timestamp || new Date())
 
-  analytics.track(body, function (err, response, data) {
-    debug('Track', err, response)
-  })
-  res.send({
-    event: 'track',
-    message: 'success'
-  })
+  try {
+    analytics.track(body, function (err, response, data) {
+      res.send({
+        event: 'track',
+        message: 'success'
+      })
+      debug('Track', err, response)
+    })
+  } catch (err) {
+    return res.send(err)
+  }
 })
 
 app.post('/alias', function (req, res, next) {
   // already sanitized
   var body = req.body
+  body.timestamp = new Date(body.timestamp)
 
-  analytics.alias(body, function (err, response, data) {
-    debug('Alias', err, response)
-  })
-  res.send({
-    event: 'alias',
-    message: 'success'
-  })
-
+  try {
+    analytics.alias(body, function (err, response, data) {
+      res.send({
+        event: 'alias',
+        message: 'success'
+      })
+      debug('Alias', err, response)
+    })
+  } catch (err) {
+    return res.send(err)
+  }
 })
 
 app.post('/screen', function (req, res, next) {
   // already sanitized
   var body = req.body
+  body.timestamp = new Date(body.timestamp)
 
   // THIS WILL ERROR, DOES NOT EXIST
   // Use as demo for errors
-  return res.sendStatus(501)
+  return res.send({event: 'screen', message: 'deprecated'})
 
   analytics.screen(body, function (err, response, data) {
+    res.send({
+      event: 'screen',
+      message: 'success'
+    })
     debug('Screen', err, response)
-  })
-  res.send({
-    event: 'screen',
-    message: 'success'
   })
 })
 
 app.post('/page', function (req, res, next) {
   // already sanitized
   var body = req.body
+  body.timestamp = new Date(body.timestamp)
 
-  analytics.page(body, function (err, response, data) {
-    debug('Page', err, response)
-  })
-  res.send({
-    event: 'screen',
-    message: 'success'
-  })
-
+  try {
+    analytics.page(body, function (err, response, data) {
+      res.send({
+        event: 'screen',
+        message: 'success'
+      })
+      debug('Page', err, response)
+    })
+  } catch (err) {
+    return res.send(err)
+  }
 })
 
 app.post('/group', function (req, res, next) {
   // already sanitized
   var body = req.body
+  body.timestamp = new Date(body.timestamp)
 
-  analytics.group(body, function (err, response, data) {
-    debug('Group', err, response)
-  })
-  res.send({
-    event: 'group',
-    message: 'success'
-  })
+  try {
+    analytics.group(body, function (err, response, data) {
+      res.send({
+        event: 'group',
+        message: 'success'
+      })
+      debug('Group', err, response)
+    })
+  } catch (err) {
+    return res.send(err)
+  }
 })
 
+app.use(function handleError (err, req, res) {
+  console.log('ERROR', err)
+  if (err) {
+    return res.send(err.message || err)
+  }
+  debug('handleError::', 'Should not log here')
+// return res.send(err)
+})
 
-//app.use(function handleError(err, req, res, next) {
-//  console.log('ERROR', err)
-//  if (err) {
-//    return res.send(err.message || err)
-//  }
-//  debug('handleError::', 'Should not log here')
-//  //return res.send(err)
-//})
-
-app.start = function start(pre, logger) {
+app.start = function start (pre, logger) {
   return new Promise(function (resolve, reject) {
     try {
       app.listen(PORT, function () {
